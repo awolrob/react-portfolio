2990e546514e8ee2f9d8d690b37cdab88738e008
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-10-08 18:55:43 -0400

    set up about me component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -27,8 +27,12 @@ function About() {
               <div className="col-md-9">
                 <p>
                   Full Stack Web Developer with the passion to bring experiences from an in-house Visual Basic / Access database
-                  environment to a modern full stack enterprise environment.I am accustomed to pushing comfort zones to achieve
-                  positive results.Having created a diverse and proven technical skill set and IT background I chose to push my
+                  environment to a modern full stack enterprise environment. I am accustomed to pushing comfort zones to achieve
+                  positive results.
+                </p>
+                <br />
+                <p>
+                  Having created a diverse and proven technical skill set and IT background, I chose to push my
                   comfort zone once again to participate in the Full Stack Web Development boot camp program at CWRU to broaden my
                   technical skill set with challenging Full Stack technologies. Through the program I found I excel in JavaScript,
                   Node.js, jQuery and HTML.
@@ -36,10 +40,16 @@ function About() {
                 <br />
                 <p>
                   I have brought about new ideas and problem-solving techniques that have produced successful results in critical
-                  manufacturing and banking applications.I am excited to leverage my existing skill set and background with my
+                  manufacturing and banking applications.
+                </p>
+                <br />
+                <p>
+                  I am excited to leverage my existing skill set and background with my
                   new Full Stack Web Development skill set to participate in building mission critical applications for the organization
                   which I affiliate.
                 </p>
+                <br />
+
                 <div >
                   <p style={{ fontSize: ".75em", textAlign: "right" }}>Define your life, not your comfort zone!</p>
                 </div>
@@ -57,7 +67,7 @@ function About() {
           </div>
         </div>
       </div>
-    </section>
+    </section >
     // <ImageBackground source={require({ coverImage })} style={{ width: '100%', height: '100%' }}>
     //   <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center' }}>
     //     <Text>Centered text</Text>
